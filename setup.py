f89a9567a650286b51f5d8381cd75a817592f929
Tomek Pulkiewicz <tomek@pulkiewicz.com>
2021-05-26 19:16:31 +0200

    adding .gitignore

--- setup.py
+++ setup.py
@@ -5,76 +5,10 @@ import os
 here = os.path.abspath(os.path.dirname(__file__))
 
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
-    long_description = "\n" + fh.read()
+    LONG_DESCRIPTION = "\n" + fh.read()
 
-VERSION = '0.0.0'
+VERSION = '0.0.2'
 DESCRIPTION = 'An open-source python WebFramework.'
-LONG_DESCRIPTION = """
-
-# [PyWebby](https://pypi.org/project/pywebby/0.0.1/)
-
--------------------
-
-### Table of Contents
-You're sections headers will be used to reference location of destination.
-
-- [Description](#description)
-- [Getting Started](#getting-started)
-- [Contributors](#contributors)
-- [License](#license)
-
-
----
-
-### Desciption
-An open source python web framework. Build on top of build in `http` module in python
-
-#### Tech used
-- Python
-- Regex
-
-
-### Getting Started
-
-#### Instalation
-```sh
-pip install pywebby
-```
-
-#### Simple application
-```py
-from webby import WebServer
-
-class HelloServer(WebServer):
-
-     @WebServer.path("/")
-     def main():
-          return { "message": "hi"}
-
-     @WebServer.path("/ping")
-     def ping():
-          return { "message": "pong"}
-
-if __name__ == "__main__":
-     my_server = HelloServer()
-     my_server.run(port=8888,autorun=True)
-
-```
-
-### Contributors 
-* [Tomek](https://github.com/TomekPulkiewicz)
-
-* [Bruno Pio](https://github.com/KaZe-Python)
-
-### License
-
-Look [here](https://github.com/TomekPulkiewicz/webby/blob/master/LICENSE)
-
-
-
-
-"""
-
 # Setting up
 setup(
     name="pywebby",
