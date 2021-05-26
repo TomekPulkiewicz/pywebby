3716f1731f8e9f55d01e1f2998eef4355cd16761
KaZe-Python <bpcosentini239@gmail.com>
2021-05-26 22:57:55 +0200

    Code Refactor & Type Hinting

--- pywebby/lib.py
+++ pywebby/lib.py
@@ -1,6 +1,6 @@
-import http.server
-import socketserver
-import sys
+from http.server import SimpleHTTPRequestHandler
+from types_ import F, FGeneric
+from socketserver import TCPServer
 
 class WebServer:
 
@@ -9,8 +9,8 @@ class WebServer:
 
      routes = []
 
-     def __init__(self):
-          class HttpHandlerClass(http.server.SimpleHTTPRequestHandler):
+     def __init__(self, address: str = "0.0.0.0", port: int = 80) -> None:
+          class HttpHandlerClass(SimpleHTTPRequestHandler):
                def do_GET(self_,):
                     for route in routes:
                          if self_.path == route["path"]:
@@ -22,16 +22,18 @@ class WebServer:
 
           
           self.http_handler = HttpHandlerClass
+          self.host = address
+          self.port = port
 
 
-     def run(self,port):
-          with socketserver.TCPServer(("", port), self.http_handler) as httpd:
-               print(f"[STARTING] Server is starting at port: {port}")
+     def run(self):
+          with TCPServer((self.host, self.port), self.http_handler) as httpd:
+               print(f"[STARTING] Server is starting at port: {self.port}")
                httpd.serve_forever()
 
      @classmethod     
-     def route(_,path_):
-          def inner(func):
+     def route(_,path_: str) -> F[[FGeneric], None]:
+          def inner(func: FGeneric) -> None:
                routes.append({"path":path_,"function":func})
 
           return inner
@@ -49,5 +51,11 @@ class HelloServer(WebServer):
 
 
 if __name__ == "__main__":
-     my_server = HelloServer()
-     my_server.run(8888)
+     # Default configuration will be host: 0.0.0.0 and port 80
+     # You can always override this, by passing either address or port
+     # or even both to the server declaration
+
+     # E.g. I'm changing only the port from 80 to 8888
+
+     my_server = HelloServer(port=8888)
+     my_server.run()
