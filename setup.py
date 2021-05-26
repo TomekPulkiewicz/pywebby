31b74eace334b8e76738e704630671389f4c57ee
Tomek Pulkiewicz <tomek@pulkiewicz.com>
2021-05-26 18:02:07 +0200

    setting up for PYPI

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'An open-source python WebFramework.'
+LONG_DESCRIPTION = 'An open source python web framework. Build on top of build in `http` module in python'
+
+# Setting up
+setup(
+    name="pywebby",
+    version=VERSION,
+    author="Tomek Pulkiewicz",
+    author_email="tomek@pulkiewicz.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'web', 'web framework','sockets'],
+    classifiers=[
+        "Development Status :: 1 - Developing",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
